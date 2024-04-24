af87acdcb562883718c26e8c797c74cd1a94f4ce
Mayank <166874794+Mayank7371@users.noreply.github.com>
2024-04-24 13:23:21 +0530

    Update a.ts

--- a.ts
+++ a.ts
@@ -2,8 +2,24 @@ const x: number = 1;
 console.log(x);
 const y: string = "mayank";
 console.log(y);
+// greeting to the user
 
 function greet(myName: string) {
   console.log("Hello " + myName);
 }
 greet("mayank");
+// return type specification
+function sum(a: number, b: number): number {
+  return a + b;
+}
+const answer = sum(1, 2);
+console.log(answer);
+
+//age verification
+function isLegal(age: number): any {
+  if (age >= 18) {
+    return true;
+  } else return false;
+}
+const answer = isLegal(21);
+console.log(answer);
